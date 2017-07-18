ee125de3e0ec58f7a67eba402614d9d5062d4fd2
Arnaud CHASSEUX <arnaud.chasseux@mullenlowe.com>
2017-07-18 11:52:26 +0200

    add test for route /api/v1/partner/myaudiuser/

--- tests/functional/PartnerBundle/Controller/ApiControllerTest.php
+++ tests/functional/PartnerBundle/Controller/ApiControllerTest.php
@@ -32,5 +32,20 @@ class ApiControllerTest extends WebTestCase
         $responseData = json_decode($response->getContent(), true);
 
         $this->assertEquals(47, $responseData['registryUserId']);
+        $this->assertEquals('GARAGE CLERFOND SARL', $responseData['commercialName']);
+    }
+
+    public function testGetPartnerByMyaudiUserId()
+    {
+        static::$client->request('GET', '/api/v1/partner/myaudiuser/55');
+        $response = static::$client->getResponse();
+
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertTrue($response->headers->contains('Content-Type', 'application/json'));
+        $this->assertJson($response->getContent());
+
+        $responseData = json_decode($response->getContent(), true);
+
+        $this->assertEquals('GARAGE CLERFOND SARL', $responseData['commercialName']);
     }
 }
