e004de7b49f0812ce716229ebbba541363a24b83
Arnaud CHASSEUX <arnaud.chasseux@mullenlowe.com>
2017-09-12 14:26:42 +0200

    add test.sh for homogeneity with other projects

--- bin/test.sh
+++ bin/test.sh
@@ -1,7 +1,47 @@
 #!/usr/bin/env bash
 
-php bin/console doctrine:fixtures:load --env=test --no-interaction
+function usage {
+    echo "Usage:"
+    echo "  $0 [options]"
+    echo ""
+    echo "Options:"
+    echo "  -c, --coverage          Generate CodeCoverage HTML report into _output/coverage"
+    echo "  -o, --codecept-args     Args and options to pass to \"codecept run\" command"
+    echo "  -h, --help              Display this help message"
+    exit 1
+}
+
+CODECEPT_OPTIONS=""
+while [[ $# -gt 0 ]]
+do
+    case "$1" in
+        -c|--coverage)
+            CODECEPT_OPTIONS="$CODECEPT_OPTIONS --coverage --coverage-html"
+        ;;
+        -o|--codecept-args)
+            CODECEPT_OPTIONS="$CODECEPT_OPTIONS $2"
+            shift # pass value
+        ;;
+        -h|--help)
+            usage
+        ;;
+        *)
+            echo "The \"$1\" option does not exist."
+            usage
+            exit 1
+        ;;
+    esac
+    shift # pass argument or value
+done
+
+echo "Runing codecept with following arguments :  $CODECEPT_OPTIONS"
+
+php vendor/bin/codecept build
+
+php bin/console doctrine:schema:update --force --env=test
+
+# php bin/console doctrine:fixtures:load --env=test --no-interaction
 #
-sqlite3 tests/_data/test.sqlite .dump  > tests/_data/test.sql
+sqlite3 tests/_data/test.sqlite .dump > tests/_data/test.sql
 #
-php vendor/bin/codecept run #--coverage --coverage-html
+php vendor/bin/codecept run $CODECEPT_OPTIONS
