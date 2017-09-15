31e11d174a94d96a713e442e3c80d361c38df929
Arnaud CHASSEUX <arnaud.chasseux@mullenlowe.com>
2017-09-15 11:25:43 +0200

    Add  DateTimeNormalizer() for json serializer

--- src/PartnerBundle/Controller/ApiController.php
+++ src/PartnerBundle/Controller/ApiController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 use Swagger\Annotations as SWG;
@@ -66,7 +67,7 @@ class ApiController extends Controller
         });
         $normalizer->setIgnoredAttributes(['partner']);
 
-        $this->serialiser = new Serializer([$normalizer], [new JsonEncoder()]);
+        $this->serialiser = new Serializer([new DateTimeNormalizer(), $normalizer], [new JsonEncoder()]);
     }
 
     /**
