3964668db1fe6433e13aed401e2c006d40612481
Raldo CHEA <raldo@chea.io>
2018-02-13 17:40:00 +0100

    429 : link partner and registry user

--- src/PartnerBundle/Controller/PartnerRegistryUserController.php
+++ src/PartnerBundle/Controller/PartnerRegistryUserController.php
@@ -6,7 +6,10 @@ use Mullenlowe\CommonBundle\Controller\MullenloweRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\View\View;
 use Mullenlowe\CommonBundle\Exception\NotFoundHttpException;
+use PartnerBundle\Entity\PartnerRegistryUser;
+use PartnerBundle\Form\PartnerRegistryUserType;
 use Swagger\Annotations as SWG;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Rest\RouteResource("")
@@ -71,6 +74,70 @@ class PartnerRegistryUserController extends MullenloweRestController
         if (!$partnerRegistryUser) {
             throw new NotFoundHttpException(static::CONTEXT, 'PartnerRegistryUser not found');
         }
+
+        return $this->createView($partnerRegistryUser);
+    }
+
+    /**
+     * @Rest\Post("/", name="_partner_registry_user")
+     * @Rest\View(serializerGroups={"rest"})
+     *
+     * @SWG\Post(
+     *     path="/",
+     *     summary="Link partner and registry user",
+     *     operationId="postPartnerRegistryUser",
+     *     tags={"Partner Registry User"},
+     *     @SWG\Parameter(
+     *         name="partner registry user",
+     *         in="body",
+     *         required=true,
+     *         @SWG\Schema(
+     *             @SWG\Property(property="partner_id", type="integer"),
+     *             @SWG\Property(property="registry_user_id", type="integer"),
+     *             required={"partner_id", "registry_user_id"})
+     *     ),
+     *     @SWG\Response(
+     *         response=201,
+     *         description="the created link partner and registry user",
+     *         @SWG\Schema(
+     *            allOf={
+     *                 @SWG\Definition(ref="#/definitions/Context"),
+     *                 @SWG\Definition(
+     *                     @SWG\Property(property="data", type="array", @SWG\Items(ref="#/definitions/PartnerRegistryUserComplete")),
+     *                 ),
+     *             }
+     *         )
+     *     ),
+     *     @SWG\Response(
+     *         response=404,
+     *         description="not found",
+     *         @SWG\Schema(ref="#/definitions/Error")
+     *     ),
+     *     @SWG\Response(
+     *         response=500,
+     *         description="internal error",
+     *         @SWG\Schema(ref="#/definitions/Error")
+     *     ),
+     *     security={{ "bearer":{} }}
+     * )
+     *
+     * @return View
+     */
+    public function postAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $dataInput = $request->request->all();
+
+        $partnerRegistryUser = new PartnerRegistryUser();
+        $form = $this->createForm(PartnerRegistryUserType::class, $partnerRegistryUser);
+        $form->submit($dataInput);
+        // validate
+        if (!$form->isValid()) {
+            return $this->view($form);
+        }
+
+        $em->persist($partnerRegistryUser);
+        $em->flush();
 
         return $this->createView($partnerRegistryUser);
     }
