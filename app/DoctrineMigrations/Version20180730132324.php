5f36b61dca9e4c5b19b420a0e31a215ca7991cba
Florian Trouvé <florian.trouve@mullenlowe.com>
2018-07-30 16:45:32 +0200

    Add a migration to link partners and registry users (fraaxxxx audi)

---
+++ app/DoctrineMigrations/Version20180730132324.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180730132324 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $partners = Yaml::parseFile(__DIR__.'/data/partners.yml');
+
+        foreach ($partners as $registryUserId => $partnerUsername) {
+            // extract kvps and type number from username
+            list($kvpsNumber, $domain) = explode('@', $partnerUsername);
+            $type = $domain === 'audi.fr' ? 'sales' : 'afterSales';
+            $result = $this->connection->fetchAll('SELECT * FROM partner WHERE kvps_number = "'.$kvpsNumber.'" AND type = "'.$type.'";');
+            if (1 === count($result)) {
+                $this->connection->insert('partner_registry_user', [
+                    'registry_user_id' => $registryUserId,
+                    'partner_id' => $result[0]['id'],
+                    'department_id' => 14,
+                    'position_id' => 17,
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ]);
+            }
+        }
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+    }
+}
