d23bcce433686fa974191958cfabf526e6786e9b
Florian Trouvé <florian.trouve@mullenlowe.com>
2018-07-31 11:47:45 +0200

    Fixes for PR

--- app/DoctrineMigrations/Version20180730132324.php
+++ app/DoctrineMigrations/Version20180730132324.php
@@ -2,15 +2,44 @@
 
 namespace Application\Migrations;
 
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Migrations\AbortMigrationException;
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
-use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180730132324 extends AbstractMigration
+class Version20180730132324 extends AbstractMigration implements ContainerAwareInterface
 {
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * @var Connection
+     */
+    private $userConnection;
+
+    /**
+     * @param ContainerInterface|null $container
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function preUp(Schema $schema)
+    {
+        $this->userConnection = $this->container->get('doctrine.dbal.user_connection');
+    }
+
     /**
      * @inheritdoc
      */
@@ -21,22 +50,30 @@ class Version20180730132324 extends AbstractMigration
 
     /**
      * @param Schema $schema
+     *
+     * @throws AbortMigrationException
      */
     public function up(Schema $schema)
     {
-        $partners = Yaml::parseFile(__DIR__.'/data/partners.yml');
+        // get fraa prefixed users
+        $registryUserPartners = $this->userConnection->fetchAll("SELECT r.id, r.username FROM registry_user r WHERE username REGEXP \"^FRAA[[:digit:]]+@audi.(fr|com|net)$\";");
+        $department = $this->connection->fetchAll("SELECT d.id FROM company_department d WHERE legacy_id = 427;");
+        $this->abortIf(0 === count($department), '"Direction" department not found.');
 
-        foreach ($partners as $registryUserId => $partnerUsername) {
+        $position = $this->connection->fetchAll("SELECT d.id FROM company_position d WHERE legacy_id = 502;");
+        $this->abortIf(0 === count($position), '"Directeur général" position not found.');
+
+        foreach ($registryUserPartners as $registryUserPartner) {
             // extract kvps and type number from username
-            list($kvpsNumber, $domain) = explode('@', $partnerUsername);
+            list($kvpsNumber, $domain) = explode('@', $registryUserPartner['username']);
             $type = $domain === 'audi.fr' ? 'sales' : 'afterSales';
             $result = $this->connection->fetchAll('SELECT * FROM partner WHERE kvps_number = "'.$kvpsNumber.'" AND type = "'.$type.'";');
             if (1 === count($result)) {
                 $this->connection->insert('partner_registry_user', [
-                    'registry_user_id' => $registryUserId,
+                    'registry_user_id' => $registryUserPartner['id'],
                     'partner_id' => $result[0]['id'],
-                    'department_id' => 14,
-                    'position_id' => 17,
+                    'department_id' => $department[0]['id'],
+                    'position_id' => $position[0]['id'],
                     'created_at' => date('Y-m-d H:i:s'),
                     'updated_at' => date('Y-m-d H:i:s'),
                 ]);
